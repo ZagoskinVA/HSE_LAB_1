ec05b1bb70ed81c49a5288a0cb7b1d827b4e91a6
Vlad <zagoskin.3@mail.ru>
2022-10-15 21:48:04 +0300

    added preprocess and features stages

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,18 +3,30 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+from src.utils import save_as_pickle, extract_target
+import pandas as pd
+from preprocess import process_data, process_target
+import src.config as cfg
 
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.argument('output_target_filepath', type=click.Path())
+def main(input_filepath, output_filepath, output_target_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    df = pd.read_csv(input_filepath)
+    df = process_data(df)
+    if(output_target_filepath is not None and output_target_filepath != '.'):
+        df, target = extract_target(df)
+        target = process_target(target)
+        save_as_pickle(target, output_target_filepath)
+    save_as_pickle(df, output_filepath)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
